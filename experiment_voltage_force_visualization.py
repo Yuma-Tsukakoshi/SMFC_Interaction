5d6e3f90ceb1ebb2540d9f7c14049e8fc8fd11e6
Yuma-Tsukakoshi <tsukakoshi.yuuma.0910@keio.jp>
2024-08-27 15:00:13 +0900

    add: voltage, forceの出力

--- experiment_voltage_force_visualization.py
+++ experiment_voltage_force_visualization.py
@@ -4,33 +4,66 @@ import serial
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import re
 
-# 前提: 各々データの名前を識別できるようにする 保存名はforce_loggerとforce_voltageで同じ名前とする
-file_name = ""
-# 2024-12-31-50VWC-45mm-10k-50N-stable-grasp-test
-date_name = file_name.split("-")[0]
+voltage_list = [
+    "2024-08-22 15-41-24.219176-80-500N-54mm-voltage-10k-data",
+    "2024-08-22 15-45-46.032379-80-500N-54mm-voltage-10k-data",
+    "2024-08-22 15-53-56.003033-80-500N-54mm-voltage-10k-data"
+]
 
-# force_logger ディレクトリからinputで取得した名前のcsvファイルを探索してforceとtimestampを取得する
-force = pd.read_csv(f"force_logger/force-logger-{date_name}/{file_name}.csv")
-force_timestamp = force["No."]
-force_force = force["Force"]
+force_list = [
+    "2024-08-22 15-41-24.219176-80-500N-54mm-force-10k-data",
+    "2024-08-22 15-45-46.032379-80-500N-54mm-force-10k-data",
+    "2024-08-22 15-53-56.003033-80-500N-54mm-force-10k-data",
+]
 
-# force_voltage ディレクトリからinputで取得した名前のcsvファイルを探索してvoltageとtimestampを取得する
-voltage = pd.read_csv(f"force_voltage/foce-voltage-{date_name}/{file_name}.csv")
-voltage_timestamp = voltage["timestamp"]
-voltage_voltage = voltage["voltage"]
+fig, axs = plt.subplots(len(voltage_list), figsize=(18, 8 * len(voltage_list)))
 
-# それぞれのデータを結合してグラフを描画する 2軸グラフで左軸にforce, 右軸にvoltageを表示する　x軸はtimestampとする
-fig, ax1 = plt.subplots()
-ax2 = ax1.twinx()
-ax1.plot(force_timestamp, force_force, color="green")
-ax2.plot(voltage_timestamp, voltage_voltage, color="blue")
-ax1.set_xlabel("timestamp")
-ax1.set_ylabel("force", color="blue")
-ax2.set_ylabel("voltage", color="red")
+for i, (voltage_file_name, force_file_name) in enumerate(zip(voltage_list, force_list)):
+    matches = re.findall(r'-(\d+)N', voltage_file_name)
+    if matches:
+        number_before_last_N = matches[-1]
+        label_name = f"{number_before_last_N}N"
+
+    date_name = " ".join(voltage_file_name.split("-")[0:3])
+    date_list = date_name.split()[0:3]
+    date = "-".join(date_list)
+
+    voltage = pd.read_csv(f"force_voltage/force-voltage-{date}/{voltage_file_name}.csv")
+    voltage = voltage.set_index("timestamp").reset_index()
+    voltage_voltage = voltage[" voltage"]
+    ax1 = axs[i] 
+    ax2 = ax1.twinx() 
+    force = pd.read_csv(f"force_logger/force-logger-{date}/{force_file_name}.csv", skiprows=5)
+    force = force.set_index("番号").reset_index()
+    force_force = force["荷重"]
+
+    max_voltage_idx = voltage_voltage.idxmax()
+    max_force_idx = force_force.idxmax()
+    if max_voltage_idx >= max_force_idx:
+        flag = "voltage"
+    elif max_voltage_idx < max_force_idx:
+        flag = "force"
+    move_idx = abs(max_voltage_idx - max_force_idx)
+    max_idx = max(max_voltage_idx, max_force_idx)
+    # max_idxの大きい方にデータのindexを増やし、移動させた分は0で埋める　そして、データのindexを再構成してほしい
+    if flag == "voltage":
+        force_force = pd.concat([pd.Series([force_force.min()] * move_idx), force_force], ignore_index=True)
+        force_force = force_force[:len(voltage_voltage)]
+    elif flag == "force":
+        voltage_voltage = pd.concat([pd.Series([voltage_voltage.min()] * move_idx), voltage_voltage], ignore_index=True)
+        voltage_voltage = voltage_voltage[:len(force_force)]
+
+    ax1.plot(voltage_voltage.index, voltage_voltage, label=f"Voltage Test {i+1}", color="blue", alpha=0.6)
+    ax2.plot(force_force.index, force_force, label=f"Force Test {i+1}", color="green", alpha=0.6)
+
+    ax1.set_xlabel("time")
+    ax1.set_ylabel("voltage", color="blue")
+    ax2.set_ylabel("force", color="green")
+    ax1.legend(loc='upper left')
+    ax2.legend(loc='upper right')
+    ax1.grid()
+
+plt.suptitle("Voltage and Force Stability Test")
 plt.show()
-
-# グラフの描画後imgディレクトリを作成してその中に日付のディレクトリを作成してその中にグラフの画像を保存する　保存名はcsvファイルと同じ名前とする
-os.makedirs("img", exist_ok=True)
-os.makedirs(f"img/{datetime.datetime.now().strftime('%Y-%m-%d')}", exist_ok=True)
-plt.savefig(f"img/{datetime.datetime.now().strftime('%Y-%m-%d')}/{file_name}.png")
